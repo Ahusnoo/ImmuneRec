c7e11bd4fda084613bca7bfd15fee3517ef90154
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-02-12 14:37:16 +0400

    Add files via upload

---
+++ Python_scripts/SNPCount.py
@@ -0,0 +1,32 @@
+import os
+
+dir = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/SNPDensity/output/'
+g_list = os.listdir(dir)
+for g in g_list:
+	idir = dir + g + '/'
+	ofile_path = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/SNPDensity/SNPDensityProcessing/'+ g + '_SNPCount.csv'
+	ofile = open(ofile_path,'w')
+	header = 'GeneName,SNPCount,Population\n'
+	ofile.write(header)
+	ifile_list = os.listdir(idir)
+	for file_name in ifile_list:
+		rm_pop = file_name [13:]
+		find_pos = rm_pop.find('_')
+		gene_name = rm_pop [:find_pos]
+		ifile_path = idir + file_name
+		i_file = open (ifile_path,'r')
+		i_file_data = i_file.readlines()
+		SNP_count_list = []
+		for line in i_file_data:
+			line_list = line.split('\t')
+			SNP = line_list[2]
+			if SNP == '1':
+				SNP_int = int(SNP)
+				SNP_count_list.append(SNP_int)
+				Total_SNP_count = sum(SNP_count_list)
+				str_Total_SNP_count = str(Total_SNP_count)
+		output_line = gene_name + ',' + str_Total_SNP_count + ',' + 'Baylor_AFR\n'
+		ofile.write (output_line)
+		
+		i_file.close()
+	ofile.close()
