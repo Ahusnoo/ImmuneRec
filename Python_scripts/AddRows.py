d8c6ea0100e823e1f26090f077b180daef51cd10
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 21:00:04 +0400

    Update AddRows.py

--- Python_scripts/AddRows.py
+++ Python_scripts/AddRows.py
@@ -1,12 +1,14 @@
 import os
 
-idir = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/SNPDensity/SNPDensityProcessing/SNPDenCalc/'
+idir = '/SNPDenCalc/'
 file_list = os.listdir(idir)
 for ifilename in file_list:
 	ifilepath = idir + ifilename
 	ifile = open(ifilepath,'r')
-	ofilepath = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/SNPDensity/SNPDensityProcessing/AddRows/' + ifilename
+	
+	ofilepath = '/AddRows/' + ifilename
 	ofile = open(ofilepath,'w')
+	
 	header = 'Gene,Population,SNPDensity\n'
 	ifiledata = ifile.readlines()
 	gene_pop_list = []
@@ -18,16 +20,18 @@ for ifilename in file_list:
 		pop = line_list[2]
 		gene_pop = gene + ',' + pop
 		gene_pop_value = gene_pop + ',' + value + '\n'
+#Write output for gene with SNP density value > 0
 		ofile.write(gene_pop_value)
 		gene_pop_list.append(gene_pop)
 	rm_ext = ifilename [:-15]
-	gene_names_filepath = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/2023/4.April/GeneNamesPerClass/' + rm_ext + '.txt'
+	gene_names_filepath = '/GeneNamesPerClass/' + rm_ext + '.txt'
 	gene_names_file = open(gene_names_filepath,'r')
 	gene_names_data = gene_names_file.readlines()
 	GENE_POP_LIST = []
 	for LINE in gene_names_data:
 		GENE = LINE.strip('\n')
 		GENE_POP = GENE + ',Baylor_AFR'
+#Write output for gene with SNP density value = 0
 		if GENE_POP not in gene_pop_list:
 			output_line = GENE_POP + ',' + '0\n'
 			ofile.write(output_line)
