01bd8cb41550a52bfed83e0c4e608150cf5fa819
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 21:59:49 +0400

    Update Reshape_dataframe.R

--- R_scripts/Reshape_dataframe.R
+++ R_scripts/Reshape_dataframe.R
@@ -1,17 +1,12 @@
-
 library(tidyr)
-library(UpSetR)
-Input_file_directory <- '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/2023/8.August/Haploview/AllSubpops/Haplotypes/Haplotypes_LongDataframes/'
-Gene_class_list <- list('Chemokine_receptors','Hemopoietic_Cytokine_Receptors','Housekeeping_Genes','IFN_Receptors','IL_Receptors','LILRs','T_cell_receptor_AB','T_cell_receptor_DG','TGFBR','TLRs','TNFR')
+
+Gene_class_list <- list('Chemokine_receptors','Hemopoietic_Cytokine_Receptors','Housekeeping_Genes','IFN_Receptors','IL_Receptors','LILRs','T_cell_receptor_AB_joining','T_cell_receptor_AB_variable','T_cell_receptor_DG_joining','T_cell_receptor_DG_variable','TGBFR','TLRs','TNFR')
+
 for (gene_class in Gene_class_list){
-	Input_file_directory <- '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/2023/8.August/Haploview/AllSubpops/Haplotypes/Haplotypes_LongDataframes/'
+	Input_file_directory <- '/Haplotypes_LongDataframes/'
 	Input_file_path <- paste (Input_file_directory,gene_class,'_Haplotypes_DF.csv', sep ='')
 	df <- read.csv (Input_file_path, header= TRUE)
-	df
 	new_df <- spread(df, key=Population, value=Value)
-	new_df
-	Output_file_path <- paste ('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/2023/8.August/Haploview/AllSubpops/Haplotypes/Haplotypes_ShortDataframes/',gene_class,'_Haplotypes_ShortDataframes.csv', sep='')
+	Output_file_path <- paste ('/Haplotypes_ShortDataframes/',gene_class,'_Haplotypes_ShortDataframes.csv', sep='')
 	write.csv (new_df,Output_file_path,row.names = FALSE)
-	x <- upset(new_df, keep.order=TRUE, main.bar.color='darkblue')
-	x
 }
