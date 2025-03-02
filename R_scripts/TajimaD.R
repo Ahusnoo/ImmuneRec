27ba1db4712cfabf94701aa54f2a99782d1e3a49
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 22:05:31 +0400

    Update TajimaD.R

--- R_scripts/TajimaD.R
+++ R_scripts/TajimaD.R
@@ -1,6 +1,6 @@
 library(PopGenome)
 
-GENOME.class <- readVCF("/Input_vcf/input.vcf.gz", 10000,"Chr", 33324387, 33359864, include.unknown=FALSE, approx=FALSE, parallel=FALSE)
+GENOME.class <- readVCF("/Input_vcf/genename.vcf.gz", 10000,Chromosome, First exon start position, Last exon end position,  include.unknown = FALSE, approx = FALSE, parallel = FALSE)
 neutrality_stats <- neutrality.stats(GENOME.class)
 Tajima <- neutrality_stats@Tajima.D
-outputfile <-write.csv(Tajima, "/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/Tajima/output/IFN_Receptors/Baylor_1KAFR_IFNAR1_Tajima.csv", row.names = FALSE)
+outputfile <-write.csv(Tajima, "/Output_Tajima/tajima.csv", row.names = FALSE)
