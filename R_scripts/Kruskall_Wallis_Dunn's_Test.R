2d161b783d5a4d4c4677ade78337a8d5672867b1
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 21:50:32 +0400

    Update Kruskall_Wallis_Dunn's_Test.R

--- R_scripts/Kruskall_Wallis_Dunn's_Test.R
+++ R_scripts/Kruskall_Wallis_Dunn's_Test.R
@@ -5,13 +5,16 @@ library(readr)
 library(FSA)
 gene_class_list <- list ('Chemokine_receptors','Hemopoietic_Cytokine_Receptors','Housekeeping_Genes','IFN_Receptors','IL_Receptors','LILRs','T_cell_receptor_AB_joining','T_cell_receptor_AB_ variable','T_cell_receptor_DG_joining','T_cell_receptor_DG_variable','TGBFR','TLRs','TNFR')
 for (gene_class in gene_class_list){
-  input_file_path <- paste("/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/MainPops/SNPDensity/Mainpops/",gene_class,"_SNPDensity_mainpops.csv",sep = "")
+  input_file_path <- paste("/SNPDensity/",gene_class,"_SNPDensity_mainpops.csv",sep = "")
   df <- read.csv(input_file_path, header = TRUE)
+  #get mean, median, quartile values
   data_summary <- tapply(df$SNPDensity, df$Population, summary)
   data_summary
+  #kruskal wallis test
   Kruskal_Wallis <- kruskal.test(SNPDensity ~ Population, data=df)
   Kruskal_Wallis
   df$Population <- as.factor(df$Population)
+  #Dunn's test
   DT = dunnTest(SNPDensity ~ Population, data=df, method="bh")
   DT
   PT = DT$res
@@ -22,6 +25,6 @@ for (gene_class in gene_class_list){
 #  Grouping <- cldList(p.value ~ Comparison, data=DTT)
 #  Grouping
   Output <- c("AFR, AMR, EAS, EUR, SAS", '\n', data_summary,'\n', Kruskal_Wallis,'\n', Grouping)
-  output_file_path <- paste("/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/MainPops/SNPDensity/KruskalWallis/",gene_class,".txt",sep = "")
+  output_file_path <- paste("/KruskalWallis/",gene_class,".txt",sep = "")
   writeLines(as.character(Output), output_file_path)
 }
