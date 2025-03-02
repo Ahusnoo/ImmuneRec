8efc8c21e489fe7fc4b4c8b545273dc9dfc1241c
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 21:32:25 +0400

    Update SNPDenCalc.py

--- Python_scripts/SNPDenCalc.py
+++ Python_scripts/SNPDenCalc.py
@@ -1,15 +1,15 @@
 import os
 
-Exon_length_file = open('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/ExonLengthPerGene/Total_Exon_Length_AllGenes.txt','r')
+Exon_length_file = open('Total_Exon_Length_AllGenes.txt','r')
 Exon_length_file_data = Exon_length_file.readlines()
 
-idir = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/SNPDensity/SNPDensityProcessing/SNPCount/'
+idir = '/SNPCount/'
 file_list = os.listdir(idir)
 for file_name in file_list:
 	ifile_path = idir + file_name
 	ifile = open(ifile_path,'r')
 	rm_ext = file_name[:-9]
-	ofile_path = '/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/Baylor_AFR/SNPDensity/SNPDensityProcessing/SNPDenCalc/' + rm_ext + 'Density.csv'
+	ofile_path = '/SNPDenCalc/' + rm_ext + 'Density.csv'
 	ofile = open(ofile_path,'w')
 	header = 'Gene,SNPDensity,Population\n'
 	ofile.write(header)
@@ -21,12 +21,14 @@ for file_name in file_list:
 		SNP_count = line_list [1]
 		population = line_list[2]
 		int_SNP_count = int(SNP_count)
+#Find size of all exons (total exon length)
 		for LINE in Exon_length_file_data:
 			RM_NEWLINE = LINE.strip('\n')
 			LINE_LIST = RM_NEWLINE.split('\t')
 			GENE_NAME = LINE_LIST[0]
 			TOTAL_EXON_LENGTH = LINE_LIST[1]
 			int_TOTAL_EXON_LENGTH = int(TOTAL_EXON_LENGTH)
+#Calculate SNP density
 			if GENE_NAME == gene_name:
 				SNPDensity = (int_SNP_count/int_TOTAL_EXON_LENGTH)*1000
 				str_SNPDensity = str(SNPDensity)
