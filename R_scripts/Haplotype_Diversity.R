b05dee5ea05b9da3b5b1c874cf3c4ac616d6377a
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 22:06:08 +0400

    Update Haplotype_Diversity.R

--- R_scripts/Haplotype_Diversity.R
+++ R_scripts/Haplotype_Diversity.R
@@ -1,6 +1,6 @@
 library(PopGenome)
 
-GENOME.class <- readVCF(genename.vcf.gz, 10000, Chromosome, First exon start position, Last exon end position,  include.unknown = FALSE, approx = FALSE, parallel = FALSE)
+GENOME.class <- readVCF("/Input_vcf/genename.vcf.gz", 10000, Chromosome, First exon start position, Last exon end position,  include.unknown = FALSE, approx = FALSE, parallel = FALSE)
 div.stat <-  diversity.stats(GENOME.class)
 hap.div <- div.stat@hap.diversity.within
 outputfile <- write.csv(hap.div, 'haplotype_diversity_genename.csv', row.names = FALSE)
