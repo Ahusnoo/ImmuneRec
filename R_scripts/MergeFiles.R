a1b520429ca2503ac5bda1e8f1d491de49a289f3
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 21:53:58 +0400

    Update MergeFiles.R

--- R_scripts/MergeFiles.R
+++ R_scripts/MergeFiles.R
@@ -1,12 +1,12 @@
 library(data.table)
 
-Gene_Class_list <- list('Chemokine_receptors','Hemopoietic_Cytokine_Receptors','Housekeeping_Genes','IFN_Receptors','IL_Receptors','LILRs','T_cell_receptor_AB','T_cell_receptor_DG','TGBFR','TLRs','TNFR')
+Gene_Class_list <- list('Chemokine_receptors','Hemopoietic_Cytokine_Receptors','Housekeeping_Genes','IFN_Receptors','IL_Receptors','LILRs','T_cell_receptor_AB_joining','T_cell_receptor_AB_variable','T_cell_receptor_DG_joining',''T_cell_receptor_DG_variable'','TGBFR','TLRs','TNFR')
 
 for (Gene_Class in Gene_Class_list){
-	input_file_directory = paste('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/MainPops/SNPDensity/', Gene_Class,sep='')
+	input_file_directory = paste('/SNPDensity/', Gene_Class,sep='')
 	file_list <- list.files(input_file_directory, pattern="*.csv", full.names=TRUE)
 	ldf <- lapply(file_list , read.csv)
 	df.combined <- do.call(rbind, ldf)
-	output_file_location = paste('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/MainPops/SNPDensity/',Gene_Class,'_SNPDensity_mainpops.csv' ,sep="")
+	output_file_location = paste(Gene_Class,'_SNPDensity_mainpops.csv' ,sep="")
 	write.csv(df.combined,output_file_location, row.names = FALSE)
 }
