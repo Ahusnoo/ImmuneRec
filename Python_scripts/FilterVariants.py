63e3dfd3d8ce86a1567f66419b6288d6b9950208
Ahusnoo <79305557+Ahusnoo@users.noreply.github.com>
2025-03-02 21:06:51 +0400

    Update FilterVariants.py

--- Python_scripts/FilterVariants.py
+++ Python_scripts/FilterVariants.py
@@ -1,6 +1,7 @@
+#list of variants subjected to signals of selection in the merged African dataset.
 gene_list = ['CCR7',  'EPOR',  'LILRA5',  'CCR4',  'CXCR4',  'ACKR2',  'GPR17',  'IL12RB1',  'IL18RAP',  'IL1R1',  'LIFR',  'TGFBR3L',  'TLR5',  'TNFRSF10B',  'TNFRSF6B',  'ACKR4',  'CNTFR',  'TNFRSF10C']
 
-with open('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/AutoimmuneDiseases/Pathogenic_variants_only/variant_summary_2023-06/variant_summary.txt', 'r') as ClinVarInputFile, open('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/GeneticDiversity/Merge_samples/MainPops/GenesStrongSelection_Diseases.txt', 'w') as ClinVarOutputFile:
+with open('/ClinVar_2023-06/variant_summary.txt', 'r') as ClinVarInputFile, open('/MainPops/GenesStrongSelection_Diseases.txt', 'w') as ClinVarOutputFile:
 	ClinVar_file_readlines = ClinVarInputFile.readlines()
 	Header = ClinVar_file_readlines[0]
 	Output_header = Header.strip('\n')
@@ -11,11 +12,13 @@ with open('/home/ahusnoo/Documents/Aaisha/MPhil_PhD/AutoimmuneDiseases/Pathogeni
 		CLINICAL_SIGNIFICANCE = LINE_LIST[6]
 		GENE_SYMBOL = LINE_LIST[4]
 		CHROMOSOME_REF = LINE_LIST[16]
+#Filter by human genome reference GRCh38
 		if CHROMOSOME_REF == 'GRCh38':
+#Filter by clinical significance
 			if CLINICAL_SIGNIFICANCE == 'Pathogenic' or  CLINICAL_SIGNIFICANCE == 'Likely pathogenic' or CLINICAL_SIGNIFICANCE == 'Pathogenic/Likely pathogenic' or CLINICAL_SIGNIFICANCE == 'protective' or  CLINICAL_SIGNIFICANCE == 'risk factor' or  CLINICAL_SIGNIFICANCE == 'association':
+#Filter by list of variants provided above
 				if GENE_SYMBOL in gene_list:
 					ClinVarOutputFile.write(LINE)
-			
-			
+	
 ClinVarInputFile.close()
 ClinVarOutputFile.close()	
